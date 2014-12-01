0d7d5488baed4dbde0c910ccdf795d0b6ea6b9ec
John Morris <john@zultron.com>
2014-12-01 12:57:05 -0600

    machinekit/rtapi/__init__.py:  collect things for easy import

--- lib/python/machinekit/rtapi/__init__.py
+++ lib/python/machinekit/rtapi/__init__.py
@@ -1,1 +1,26 @@
-from rtapi_bindings import *
+# Make main objects available from machinekit.rtapi module
+from rtapi import RTAPI, RTAPIPermissionError
+from config import Config, RTAPIConfigException, RTAPIConfigNotFoundException
+from environment import Environment, RTAPIEnvironmentInitError, \
+    RTAPIEnvironmentRLimitError, RTAPIEnvironmentPrivilegeError
+from shm import MKSHMSegment, SHMDrvAPIRuntimeError, \
+    SHMOps, RTAPISHMRuntimeError
+from util import Util
+from rtapi_global import GlobalData, RTAPIGlobalDataException
+
+rtapi_exceptions = (
+    RTAPIPermissionError,
+    RTAPIConfigException,
+    RTAPIConfigNotFoundException,
+    RTAPIEnvironmentInitError,
+    RTAPIEnvironmentRLimitError,
+    RTAPIEnvironmentPrivilegeError,
+    SHMDrvAPIRuntimeError,
+    RTAPISHMRuntimeError,
+    RTAPIGlobalDataException,
+    )
+
+
+# FIXME trash?
+from compat import Compat
+from setuid_helper import SetuidHelper
